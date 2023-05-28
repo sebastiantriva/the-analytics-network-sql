877d0a4766b2b75c0535364078722ad4d861956d
sebastiantriva <128645228+sebastiantriva@users.noreply.github.com>
2023-05-28 13:52:38 +0200

    Update fct_inventory.sql

--- parte-3/project/retail-data-warehouse/fact/fct_inventory.sql
+++ parte-3/project/retail-data-warehouse/fact/fct_inventory.sql
@@ -4,10 +4,11 @@ Conteo de inventario al inicio y final del dia por fecha, tienda y codigo
 DROP TABLE IF EXISTS fct.inventory
 
 CREATE TABLE fct.inventory
-                 (
-                              tienda  SMALLINT
-                            , sku     VARCHAR(10)
-                            , fecha   DATE
-                            , inicial SMALLINT
-                            , final   SMALLINT
-                 );
+(
+      id_inventory  INTEGER PRIMARY KEY,
+      tienda        SMALLINT,
+      sku           VARCHAR(10),
+      fecha         DATE,
+      inicial       SMALLINT,
+      final         SMALLINT
+);
