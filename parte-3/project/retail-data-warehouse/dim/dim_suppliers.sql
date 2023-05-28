527a3f0d159ce068c74dcac8d90b26a76bb29243
sebastiantriva <128645228+sebastiantriva@users.noreply.github.com>
2023-05-28 13:49:31 +0200

    Update dim_suppliers.sql

--- parte-3/project/retail-data-warehouse/dim/dim_suppliers.sql
+++ parte-3/project/retail-data-warehouse/dim/dim_suppliers.sql
@@ -3,9 +3,9 @@
 DROP TABLE IF EXISTS dim.suppliers;
 
 CREATE TABLE dim.suppliers
-              (
-                  id_supplier         INTEGER PRIMARY KEY,
-                  codigo_producto     VARCHAR (255),
-                  nombre              VARCHAR (255),
-                  is_primary          BOOLEAN
-              );
+(
+      id_supplier         INTEGER PRIMARY KEY,
+      codigo_producto     VARCHAR (255),
+      nombre              VARCHAR (255),
+      is_primary          BOOLEAN
+);
