b2de1789ba6372793fbc7a7a225f00b56de4941c
sebastiantriva <128645228+sebastiantriva@users.noreply.github.com>
2023-05-28 12:33:57 +0200

    Create dim_store_master.sql

---
+++ parte-3/project/retail-data-warehouse/dim/dim_store_master.sql
@@ -0,0 +1,18 @@
+/* Crea tabla store_master
+Tabla maestra de tiendas 
+*/
+DROP TABLE IF EXISTS dim.store_master;
+      
+CREATE TABLE dim.store_master
+                 (
+                              codigo_tienda  SMALLINT   PRIMARY KEY
+                            , pais           VARCHAR(100)
+                            , provincia      VARCHAR(100)
+                            , ciudad         VARCHAR(100)
+                            , direccion      VARCHAR(255)
+                            , nombre         VARCHAR(255)
+                            , tipo           VARCHAR(100)
+                            , fecha_apertura DATE
+                            , latitud        DECIMAL(10, 8)
+                            , longitud       DECIMAL(11, 8)
+                 );
