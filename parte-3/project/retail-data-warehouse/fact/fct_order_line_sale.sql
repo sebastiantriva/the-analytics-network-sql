5bc83f1543a6533dd2d9143aa71edc6862d7f017
sebastiantriva <128645228+sebastiantriva@users.noreply.github.com>
2023-05-28 13:01:17 +0200

    Create fct_order_line_sale.sql

---
+++ parte-3/project/retail-data-warehouse/fact/fct_order_line_sale.sql
@@ -0,0 +1,20 @@
+/* Crea tabla order_line_sales
+Ventas a nivel numero de orden, item.
+*/
+DROP TABLE IF EXISTS fct.order_line_sale;
+    
+CREATE TABLE fct.order_line_sale
+                 (
+                              orden      VARCHAR(10)      PRIMARY KEY
+                            , producto   VARCHAR(10)
+                            , tienda     SMALLINT
+                            , fecha      DATE
+                            , cantidad   INTEGER
+                            , venta      DECIMAL(18,5)
+                            , descuento  DECIMAL(18,5)
+                            , impuestos  DECIMAL(18,5)
+                            , creditos   DECIMAL(18,5)
+                            , moneda     VARCHAR(3)
+                            , pos        SMALLINT
+                            , is_walkout BOOLEAN
+                 );
