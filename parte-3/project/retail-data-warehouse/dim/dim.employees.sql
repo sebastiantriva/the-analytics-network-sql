2cf559c634a1190bb621a09eac9f6eee67ac9850
sebastiantriva <128645228+sebastiantriva@users.noreply.github.com>
2023-05-28 13:42:30 +0200

    Update dim.employees.sql

--- parte-3/project/retail-data-warehouse/dim/dim.employees.sql
+++ parte-3/project/retail-data-warehouse/dim/dim.employees.sql
@@ -3,15 +3,15 @@
 DROP TABLE IF EXISTS dim.employees;
 
 CREATE TABLE dim.employees
-          (
-              id_employee           integer NOT NULL PRIMARY KEY,
-              nombre                character varying (255) NOT NULL,
-              apellido              character varying (255) NOT NULL,
-              fecha_entrada         date NOT NULL,
-              fecha_salida          date,
-              telefono              character varying (255),
-              pais                  character varying (100) NOT NULL,
-              provincia             character varying (100) NOT NULL,
-              codigo_tienda         smallint NOT NULL,
-              posicion              character varying (255) NOT NULL
-          );
+(
+          id_employee           integer NOT NULL PRIMARY KEY,
+          nombre                character varying (255) NOT NULL,
+          apellido              character varying (255) NOT NULL,
+          fecha_entrada         date NOT NULL,
+          fecha_salida          date,
+          telefono              character varying (255),
+          pais                  character varying (100) NOT NULL,
+          provincia             character varying (100) NOT NULL,
+          codigo_tienda         smallint NOT NULL,
+          posicion              character varying (255) NOT NULL
+);
