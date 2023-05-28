a283d80f37f473bf30f564e9b92733359570b691
sebastiantriva <128645228+sebastiantriva@users.noreply.github.com>
2023-05-28 13:43:40 +0200

    Update dim_date.sql

--- parte-3/project/retail-data-warehouse/dim/dim_date.sql
+++ parte-3/project/retail-data-warehouse/dim/dim_date.sql
@@ -3,16 +3,16 @@
 DROP TABLE IF EXISTS dim.date;
 
 CREATE TABLE dim.date
-              (
-                  id_fecha              integer PRIMARY KEY,
-                  fecha                 date,
-                  anio                  integer,
-                  mes                   integer,
-                  dia_de_semana         character varying (10),
-                  is_weekend            boolean,
-                  mes_text              character varying (10),
-                  anio_fiscal           integer,
-                  anio_fiscal_text      character varying (6),
-                  fiscal_qarter         character varying (2),
-                  fecha_anio_anterior   date
-              );
+(
+        id_fecha              integer PRIMARY KEY,
+        fecha                 date,
+        anio                  integer,
+        mes                   integer,
+        dia_de_semana         character varying (10),
+        is_weekend            boolean,
+        mes_text              character varying (10),
+        anio_fiscal           integer,
+        anio_fiscal_text      character varying (6),
+        fiscal_qarter         character varying (2),
+        fecha_anio_anterior   date
+);
