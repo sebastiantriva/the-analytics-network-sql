b24b26ed788f642edcbd7e1cdf0a8da4138ca800
sebastiantriva <128645228+sebastiantriva@users.noreply.github.com>
2023-05-28 13:46:48 +0200

    Update dim_product_master.sql

--- parte-3/project/retail-data-warehouse/dim/dim_product_master.sql
+++ parte-3/project/retail-data-warehouse/dim/dim_product_master.sql
@@ -6,21 +6,21 @@ is_active indica que productos estan actualmente a la venta
 DROP TABLE IF EXISTS dim.product_master ;
     
 CREATE TABLE dim.product_master
-                 (
-                              codigo_producto VARCHAR(255) PRIMARY KEY
-                            , nombre          VARCHAR(255)
-                            , categoria       VARCHAR(255)
-                            , subcategoria    VARCHAR(255)
-                            , subsubcategoria VARCHAR(255)
-                            , material        VARCHAR(255)
-                            , color           VARCHAR(255)
-                            , origen          VARCHAR(255)
-                            , ean             bigint
-                            , is_active       boolean
-                            , has_bluetooth   boolean
-                            , talle           VARCHAR(255)
+(
+	codigo_producto VARCHAR(255) PRIMARY KEY,
+	nombre          VARCHAR(255),
+	categoria       VARCHAR(255),
+	subcategoria    VARCHAR(255),
+	subsubcategoria VARCHAR(255),
+	material        VARCHAR(255),
+	color           VARCHAR(255),
+	origen          VARCHAR(255),
+	ean             bigint,
+	is_active       boolean,
+	has_bluetooth   boolean,
+	talle           VARCHAR(255),
                      
-                            constraint fk_codigo_producto_product_master
-		                        foreign key (product_id)
-		                        references dim.order_line_sale(producto)
-                 );
+	constraint fk_codigo_producto_product_master
+	foreign key (product_id)
+	references dim.order_line_sale(producto)
+);
