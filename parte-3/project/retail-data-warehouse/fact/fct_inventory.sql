0da78d2bbd8db8b71f2e2b39264dff07f462801c
sebastiantriva <128645228+sebastiantriva@users.noreply.github.com>
2023-05-28 14:32:18 +0200

    Update fct_inventory.sql

--- parte-3/project/retail-data-warehouse/fact/fct_inventory.sql
+++ parte-3/project/retail-data-warehouse/fact/fct_inventory.sql
@@ -10,5 +10,13 @@ CREATE TABLE fct.inventory
       sku           VARCHAR(10),
       fecha         DATE,
       inicial       SMALLINT,
-      final         SMALLINT
+      final         SMALLINT,
+      
+      constraint fk_tienda_id_inventory
+      foreign key (tienda)
+      references dim.store_master (codigo_tienda),
+      
+      constraint fk_sku_id_inventory
+      foreign key (sku)
+      references dim.product_master (codigo_producto)
 );
