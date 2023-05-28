fe66806acb0d99dc994f46470ec8045d14aec24e
sebastiantriva <128645228+sebastiantriva@users.noreply.github.com>
2023-05-28 13:44:52 +0200

    Update dim_monthly_average_fx_rate.sql

--- parte-3/project/retail-data-warehouse/dim/dim_monthly_average_fx_rate.sql
+++ parte-3/project/retail-data-warehouse/dim/dim_monthly_average_fx_rate.sql
@@ -4,11 +4,11 @@ Promedio de cotizacion mensual de USD a ARS, EUR a ARS y USD a URU
 DROP TABLE IF EXISTS dim.monthly_average_fx_rate;
     
 CREATE TABLE dim.monthly_average_fx_rate
-                 (
-                            id_monthly_average_fx_rate      INTEGER PRIMARY KEY
-                            , anio                          DATE
-                            , mes                           DATE
-                            , cotizacion_usd_peso           DECIMAL
-                            , cotizacion_usd_eur            DECIMAL
-                            , cotizacion_usd_uru            DECIMAL
-                 );
+(
+        id_monthly_average_fx_rate    INTEGER PRIMARY KEY,
+        anio                          DATE,
+        mes                           DATE,
+        cotizacion_usd_peso           DECIMAL,
+        cotizacion_usd_eur            DECIMAL,
+        cotizacion_usd_uru            DECIMAL
+);
