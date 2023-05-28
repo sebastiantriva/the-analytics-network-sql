13052abb9176f2d1fd801df9ada839c4aca64427
sebastiantriva <128645228+sebastiantriva@users.noreply.github.com>
2023-05-28 13:09:42 +0200

    Update dim_suppliers.sql

--- parte-3/project/retail-data-warehouse/dim/dim_suppliers.sql
+++ parte-3/project/retail-data-warehouse/dim/dim_suppliers.sql
@@ -4,8 +4,8 @@ DROP TABLE IF EXISTS dim.suppliers;
 
 CREATE TABLE dim.suppliers
               (
-                  id_supplier         integer                   PRIMARY KEY,
-                  codigo_producto     character varying (255),
-                  nombre              character varying (255),
-                  is_primary          boolean
+                  id_supplier         INTEGER PRIMARY KEY,
+                  codigo_producto     VARCHAR (255),
+                  nombre              VARCHAR (255),
+                  is_primary          BOOLEAN
               );
