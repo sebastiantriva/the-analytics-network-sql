5d0ecb04ea58eda5609fc65120fb38326626e99d
sebastiantriva <128645228+sebastiantriva@users.noreply.github.com>
2023-05-28 14:02:03 +0200

    Update fct_returns.sql

--- parte-3/project/retail-data-warehouse/fact/fct_returns.sql
+++ parte-3/project/retail-data-warehouse/fact/fct_returns.sql
@@ -3,14 +3,14 @@
 DROP TABLE IF EXISTS fct.return_movements;
 
 CREATE TABLE fct.return_movements
-              (
-                  orden_venta     character varying (10)    NOT NULL,
-                  envio           character varying (6)     NOT NULL,
-                  item            character varying (7)     NOT NULL,
-                  cantidad        smallint                  NOT NULL,
-                  id_movimiento   integer                   NOT NULL PRIMARY KEY,
-                  desde           character varying (255)   NOT NULL,
-                  hasta           character varying (255)   NOT NULL,
-                  recibido_por    character varying (255),
-                  fecha           date                      NOT NULL
-              );
+(
+      id_movimiento   integer                   NOT NULL PRIMARY KEY,
+      orden_venta     character varying (10)    NOT NULL,
+      envio           character varying (6)     NOT NULL,
+      item            character varying (7)     NOT NULL,
+      cantidad        smallint                  NOT NULL,
+      desde           character varying (255)   NOT NULL,
+      hasta           character varying (255)   NOT NULL,
+      recibido_por    character varying (255),
+      fecha           date                      NOT NULL
+);
